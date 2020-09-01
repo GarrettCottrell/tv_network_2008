789759d60ae7c8a38e5e5328b1895c650cc0bb8e
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-09-01 10:16:53 -0600

    finished with main_characters method

---
+++ lib/network.rb
@@ -0,0 +1,24 @@
+require './lib/character'
+require './lib/show'
+
+class Network
+  attr_reader :name, :shows
+
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def main_characters
+    main_characters_variable = @shows.map do |show|
+      show.characters.find_all do |character|
+        show.characters
+      end
+    end
+    main_characters_variable.flatten
+  end
+end
